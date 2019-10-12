39aed73366aca9c12036157bfafb434411692644
Thomas Li <guslithd@student.gu.se>
2019-10-12 17:28:42 +0200

    Added instance of Show for HangWord

--- src/Words.hs
+++ src/Words.hs
@@ -1,17 +1,31 @@
 {- Module for handling importing of words from an external file,
    providing functions for getting random words etc. -}
 
+{-# LANGUAGE FlexibleInstances #-}
+
 module Words
     ( HangWord
-    , Letter
+    , Letter (Visible, Hidden)
     , hangWord
     , importWords
     , randomWord
+    , toChar
     ) where
+
+import Data.List (intersperse)
 
 type HangWord = [Letter]
 
 data Letter = Visible Char | Hidden Char
+    deriving (Eq, Show)
+
+instance {-# OVERLAPPING #-} Show HangWord where
+    show xs = intersperse ' ' $ map toChar xs
+
+-- | Converts a Letter to the corresponding Char representation.
+toChar :: Letter -> Char
+toChar (Visible c) = c
+toChar (Hidden _)  = '_'
 
 -- | Parses and imports words from an external source (such as a text file).
 importWords :: IO [String]
