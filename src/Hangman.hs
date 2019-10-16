fe2162058ee18909d6db22cfc9e45103f5c87e43
Thomas Li <guslithd@student.gu.se>
2019-10-16 21:26:12 +0200

    Added input loop with validation

--- src/Hangman.hs
+++ src/Hangman.hs
@@ -1,5 +1,25 @@
-module Hangman where
+module Hangman
+    ( play
+    ) where
+
+import Data.Char (isAlpha)
 
 play :: IO ()
-play = undefined
+play = do
+    letter <- getInput
 
+    -- TODO: Logic for updating state depending on guessed letter
+    play
+
+getInput :: IO Char
+getInput = do
+    putStr "Input a letter > "
+    input <- getLine
+
+    -- Return char on valid input, otherwise prompt again.
+    maybe getInput return $ validateString input
+
+validateString :: String -> Maybe Char
+validateString xs = fromBool (valid xs) (head xs)
+    where valid xs = length xs == 1 && isAlpha (head xs)
+          fromBool p = if p then Just else const Nothing
