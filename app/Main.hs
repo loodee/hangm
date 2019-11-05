69651fe010fed00db6249bea8f9b4b3d6f5cb418
Thomas Li <guslithd@student.gu.se>
2019-11-05 13:12:29 +0100

    Arg instances for help, words, lives

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,38 @@ module Main where
 
 import System.IO
 
+import System.Console.ParseArgs
+
 import Hangman (GameState (GameState), play)
 import Words (importWords, randomWord)
 
 -- TODO: Get these from program args instead
 lives = 7
 wordsFile = "words.txt"
+
+argHelp = Arg
+    { argIndex = "help"
+    , argAbbr = Just 'h'
+    , argName = Just "help"
+    , argData = argDataOptional "help" ArgtypeString
+    , argDesc = "Show options."
+    }
+
+argLives = Arg
+    { argIndex = "lives"
+    , argAbbr = Nothing
+    , argName = Just "lives"
+    , argData = argDataDefaulted "lives" ArgtypeInt 7
+    , argDesc = "Number of starting lives."
+    }
+
+argWords = Arg
+    { argIndex = "words"
+    , argAbbr = Nothing
+    , argName = Just "words"
+    , argData = argDataDefaulted "words" ArgtypeString "words.txt"
+    , argDesc = "Location of file containing potential words."
+    }
 
 main :: IO ()
 main = do
